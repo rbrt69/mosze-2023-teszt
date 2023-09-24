3aa0d1afae9822639ab293bbb0b4483d1d96f017
rbrt69 <145350166+rbrt69@users.noreply.github.com>
2023-09-24 18:10:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    int *b = new int[NELEMENTS]; //Rossz megnevezés
+    std::cout << '1-100 ertekek duplazasa' //hiányzó pontosvessző 
+    for (int i = 0;) //for nincs folytatva 
+    {
+        b[i] = i * 2;
+    }
+    for (int i = 0; i; i++) //i<N_ELEMENTS
+    {
+        std::cout << "Ertek:" //hiányzó érték kiiratás
+    }    
+    std::cout << "Atlag szamitasa: " << std::endl;
+    int atlag; //nincs értéke
+    for (int i = 0; i < N_ELEMENTS, i++)// N_ELEMENTS után pontosvesszőt kell rakni
+    {
+        atlag += b[i]//hiányzó pontosvessző
+    }
+    atlag /= N_ELEMENTS;
+    std::cout << "Atlag: " << atlag << std::endl;
+    return 0;
+}
+
+//hibák javítása 
+#include <iostream>
+
+constexpr int N_ELEMENTS = 100;
+
+int main()
+{
     int *b = new int[N_ELEMENTS]; 
     std::cout << "1-100 ertekek duplazasa" << std::endl;
 
@@ -28,4 +56,4 @@ int main()
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
 return 0;
-{
+}
